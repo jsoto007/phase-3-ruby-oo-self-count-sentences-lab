1d09f26a32ca7cfd11f3773735f5d789ff65caf4
Jose Soto <jsoto7087@gmail.com>
2023-01-20 02:46:44 -0500

    commit tree 9f8a173790a68823d42495f5c6b69acd756a248c

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    regex_filter = /(\! |\. |\? )/
+    new_sentense = self.split(regex_filter)
+    
+    output = []
+    new_sentense.each do |item|
+    next if item =~ regex_filter
+    output << item
+    end 
+    output.count
   end
 end
